da76ef4df408fbca3f5f23084fa1bfc4049e3cdb
aziz <azizz_belkacemi@yahoo.com>
2023-02-10 15:32:12 +0100

    upload cos

--- cos_release/api_v1/admin.py
+++ cos_release/api_v1/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import File
 
-# Register your models here.
+class FileAdmin(admin.ModelAdmin):
+    list_display = ('file', 'file_name')
+
+    def file_name(self, obj):
+        return obj.file.name
+
+    file_name.short_description = 'File Name'
+
+admin.site.register(File, FileAdmin)
--- cos_release/api_v1/urls.py
+++ cos_release/api_v1/urls.py
@@ -4,4 +4,5 @@ from .views import upload_file, download_file, delete_file
 urlpatterns = [
     path('upload/', upload_file, name='upload_file'),
     path('download/<str:file_name>/', download_file, name='download_file'),
-    path('delete/<str:file_name>/', delete_file, name
+    path('delete/<str:file_name>/', delete_file,  name='delete_file'),
+]
