14aa37b8fedf5368b6d83a19ecd5047b22257146
aziz <azizz_belkacemi@yahoo.com>
2023-02-10 15:58:37 +0100

    upload cos

--- cos_release/api_v1/views.py
+++ cos_release/api_v1/views.py
@@ -1,31 +1,25 @@
-# views.py
-from django.contrib.auth.decorators import user_passes_test
-from django.shortcuts import render
-from .models import File
+from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from ibm_botocore.client import Config
-import boto3
-from django.conf import settings
+import ibm_boto3
 
-def is_admin(user):
-    return user.is_staff
+class UploadFileView(APIView):
+    permission_classes = [permissions.IsAdminUser]
 
-@user_passes_test(is_admin)
-def upload_file(request):
-    if request.method == 'POST':
+    def post(self, request, *args, **kwargs):
         file = request.FILES['file']
 
-        s3 = boto3.resource(
-            's3',
-            aws_access_key_id=settings.COS_ACCESS_KEY_ID,
-            aws_secret_access_key=settings.COS_SECRET_ACCESS_KEY,
-            config=Config(signature_version='oauth'),
-            endpoint_url=settings.COS_ENDPOINT
+        # Configuration for IBM Cloud Object Storage
+        cos = ibm_boto3.client("s3",
+            ibm_api_key_id="your_api_key_id",
+            ibm_service_instance_id="your_service_instance_id",
+            config=Config(signature_version="oauth"),
+            endpoint_url="https://s3.us-south.cloud-object-storage.appdomain.cloud"
         )
 
-        s3.Bucket(settings.COS_BUCKET).put_object(Key=file.name, Body=file)
+        # Upload the file to the Object Storage
+        cos.upload_fileobj(file, "your_bucket_name", file.name)
 
-        uploaded_file = f"https://{settings.COS_BUCKET}.{settings.COS_ENDPOINT}/{file.name}"
-        File.objects.create(file=uploaded_file)
-
-        return render(request, 'upload.html', {'uploaded_file': uploaded_file})
-    return render(request, 'upload.html')
+        # Return response to indicate success
+        return Response({'status': 'success'})
