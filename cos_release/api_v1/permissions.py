0cc8f32a9ef972aa47d1a33f54f798001134befd
aziz <azizz_belkacemi@yahoo.com>
2023-02-15 17:00:13 +0100

    upload cos

--- cos_release/api_v1/permissions.py
+++ cos_release/api_v1/permissions.py
@@ -1,15 +1,43 @@
-from rest_framework import permissions
-
 class CanUploadFile(permissions.BasePermission):
+    BUCKET_MAP = {
+        'fudj': 'B1056yrude',
+        'etna': 'B5p5r67eD',
+        'izaru': 'B9jK2uf84',
+        'vesuve': 'B6mS2jpQ7',
+    }
+
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.groups.filter(name='admin').exists()
+        bucket_name = view.kwargs.get('bucket_name')
+        if not bucket_name:
+            return False
+
+        if request.user.is_authenticated and request.user.groups.filter(name='admin').exists():
+            return True
+
+        real_bucket_name = self.BUCKET_MAP.get(bucket_name)
+        if not real_bucket_name:
+            return False
+
+        can_upload = request.user.has_perm('upload_file', real_bucket_name)
+        return can_upload
+
 
 class CanDownloadFile(permissions.BasePermission):
+    BUCKET_MAP = {
+        'fudj': 'B1056yrude',
+        'etna': 'B5p5r67eD',
+        'izaru': 'B9jK2uf84',
+        'vesuve': 'B6mS2jpQ7',
+    }
+
     def has_permission(self, request, view):
-        bucket_name = view.kwargs.get('bucket_name', None)
-        if bucket_name == 'fudji':
-            return request.user.is_authenticated and request.user.groups.filter(name='fudji').exists()
-        elif bucket_name == 'ETNA':
-            return request.user.is_authenticated and request.user.groups.filter(name='ETNA').exists()
-        else:
+        bucket_name = view.kwargs.get('bucket_name')
+        if not bucket_name:
             return False
+
+        real_bucket_name = self.BUCKET_MAP.get(bucket_name)
+        if not real_bucket_name:
+            return False
+
+        can_download = request.user.has_perm('download_file', real_bucket_name)
+        return can_download
