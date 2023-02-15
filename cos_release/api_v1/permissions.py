d5c87fceb54d091483588876546e9870e00d18bf
aziz <azizz_belkacemi@yahoo.com>
2023-02-15 16:23:26 +0100

    upload cos

--- cos_release/api_v1/permissions.py
+++ cos_release/api_v1/permissions.py
@@ -1,40 +1,15 @@
-from guardian.core import ObjectPermissionChecker
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
-
-class HasBucketAccessPermission(BasePermission):
-    """
-    Permission pour vérifier si un utilisateur a accès à un bucket spécifique.
-    """
-    def has_object_permission(self, request, view, obj):
-        checker = ObjectPermissionChecker(request.user)
-        return checker.has_perm('view_file', obj) and obj.name == view.kwargs['bucket_name']
-
-
-class HasUploadPermission(BasePermission):
-    """
-    Permission pour vérifier si un utilisateur a le droit de télécharger un fichier.
-    """
+class CanUploadFile(permissions.BasePermission):
     def has_permission(self, request, view):
-        user = request.user
-        if user.is_authenticated and user.groups.filter(name='admin').exists():
-            return True
+        return request.user.is_authenticated and request.user.groups.filter(name='admin').exists()
 
-        return False
-
-
-class HasDownloadPermission(BasePermission):
-    """
-    Permission pour vérifier si un utilisateur a le droit de télécharger un fichier.
-    """
+class CanDownloadFile(permissions.BasePermission):
     def has_permission(self, request, view):
-        user = request.user
-        bucket_name = view.kwargs.get('bucket_name')
-        if user.is_authenticated and (
-                user.groups.filter(name='admin').exists() or
-                (bucket_name == 'fudji' and user.groups.filter(name='fudji').exists()) or
-                (bucket_name == 'ETNA' and user.groups.filter(name='ETNA').exists())
-        ):
-            return True
-
-        return False
+        bucket_name = view.kwargs.get('bucket_name', None)
+        if bucket_name == 'fudji':
+            return request.user.is_authenticated and request.user.groups.filter(name='fudji').exists()
+        elif bucket_name == 'ETNA':
+            return request.user.is_authenticated and request.user.groups.filter(name='ETNA').exists()
+        else:
+            return False
