405de540602d3213a8243f89d4aa03b6d15f80e4
aziz <azizz_belkacemi@yahoo.com>
2023-02-15 15:41:40 +0100

    upload cos

--- cos_release/api_v1/models.py
+++ cos_release/api_v1/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class File(models.Model):
-    file = models.FileField(upload_to='files/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='uploads/')
+
+    def get_owner(self):
+        return self.owner
--- cos_release/api_v1/views.py
+++ cos_release/api_v1/views.py
@@ -1,25 +1,38 @@
-from rest_framework.parsers import MultiPartParser
-from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from guardian.shortcuts import get_objects_for_user
+from guardian.decorators import permission_required_or_403
+
 
 class FileView(APIView):
-    parser_classes = [MultiPartParser]
+    @permission_required_or_403('upload_file', (Bucket, 'name', 'bucket_name'))
+    def post(self, request, bucket_name, file_name):
+        # Code for file upload goes here
+        return Response({'message': 'File uploaded successfully'}, status=status.HTTP_201_CREATED)
 
-    def post(self, request, *args, **kwargs):
-        file_serializer = FileSerializer(data=request.data)
-        if file_serializer.is_valid():
-            file_serializer.save()
-            return Response(file_serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    @permission_required_or_403('download_file', (Bucket, 'name', 'bucket_name'))
+    def get(self, request, bucket_name, file_name):
+        bucket = get_object_or_404(Bucket, name=bucket_name)
+        file = get_object_or_404(File, name=file_name, bucket=bucket)
 
-    def get(self, request, *args, **kwargs):
-        file_name = kwargs.get('file_name')
-        if file_name:
-            file = File.objects.get(name=file_name)
-            file_serializer = FileSerializer(file)
-            return Response(file_serializer.data)
+        # Check permissions for different user groups
+        if request.user.groups.filter(name='admin').exists():
+            # Allow admin users to download from any bucket
+            content = file.content
+        elif request.user.groups.filter(name='fudji').exists() and bucket_name == 'fudji':
+            # Allow fudji users to download from fudji bucket only
+            content = file.content
+        elif request.user.groups.filter(name='ETNA').exists() and bucket_name == 'ETNA':
+            # Allow ETNA users to download from ETNA bucket only
+            content = file.content
         else:
-            files = File.objects.all()
-            file_serializer = FileSerializer(files, many=True)
-            return Response(file_serializer.data)
+            # Deny access to other users
+            raise PermissionDenied
+
+        response = HttpResponse(content, content_type='application/octet-stream')
+        response['Content-Disposition'] = f'attachment; filename="{file.name}"'
+        return response
