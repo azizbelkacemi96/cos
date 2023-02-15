16d585c0f7bbe0e8ac4e75b60246cae2ce922ff4
aziz <azizz_belkacemi@yahoo.com>
2023-02-15 16:26:13 +0100

    upload cos

--- cos_release/api_v1/views.py
+++ cos_release/api_v1/views.py
@@ -1,32 +1,32 @@
+from django.http import HttpResponse
 from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.http import FileResponse
-from django.shortcuts import get_object_or_404
-from django.contrib.auth.decorators import permission_required
-from guardian.decorators import permission_required_or_403
-from .models import Bucket, File
-from .permissions import AdminPermission, FudjiPermission, EtnaPermission
+from rest_framework.permissions import IsAuthenticated
+from guardian.shortcuts import assign_perm, get_objects_for_user
+from .models import File
+from .permissions import CanDownloadFile, CanUploadFile
 
 
 class FileView(APIView):
-
+    permission_classes = [IsAuthenticated]
+    permission_classes = [CanUploadFile | CanDownloadFile]
     def get(self, request, bucket_name, file_name):
-        file = get_object_or_404(File, bucket__name=bucket_name, name=file_name)
-        response = FileResponse(file.file)
-        response['Content-Disposition'] = f'attachment; filename="{file_name}"'
+        try:
+            file = File.objects.get(bucket_name=bucket_name, file_name=file_name)
+        except File.DoesNotExist:
+            return HttpResponse(status=404)
+
+        if not CanDownloadFile(request.user, file).has_permission():
+            return HttpResponse(status=403)
+
+        response = HttpResponse(file.file, content_type=file.content_type)
+        response['Content-Disposition'] = f'attachment; filename="{file.file_name}"'
         return response
 
-    @permission_required_or_403(AdminPermission, (Bucket, 'name', 'bucket_name'))
-    def post(self, request, bucket_name):
-        bucket = get_object_or_404(Bucket, name=bucket_name)
-        file_obj = File(bucket=bucket, name=request.data['file'].name, file=request.data['file'])
-        file_obj.save()
-        return Response({'detail': f'File "{file_obj.name}" has been uploaded to "{bucket.name}".'})
+    def post(self, request, bucket_name, file_name):
+        if not CanUploadFile(request.user).has_permission():
+            return HttpResponse(status=403)
 
-    @permission_required_or_403(FudjiPermission, (Bucket, 'name', 'bucket_name'))
-    @permission_required_or_403(EtnaPermission, (Bucket, 'name', 'bucket_name'))
-    def get(self, request, bucket_name):
-        bucket = get_object_or_404(Bucket, name=bucket_name)
-        files = File.objects.filter(bucket=bucket)
-        data = [{'name': file.name, 'url': f'/api/v1/report/{bucket_name}/{file.name}'} for file in files]
-        return Response(data)
+        file = File(bucket_name=bucket_name, file_name=file_name, file=request.data['file'])
+        file.save()
+        assign_perm('download_file', request.user, file)
+        return HttpResponse(status=201)
