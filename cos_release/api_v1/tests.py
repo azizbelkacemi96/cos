423390582d6d65940024e15be3475e08a9c6481a
aziz <azizz_belkacemi@yahoo.com>
2023-02-24 14:35:40 +0100

    test

--- cos_release/api_v1/tests.py
+++ cos_release/api_v1/tests.py
@@ -1,36 +1,75 @@
-from django.test import TestCase, RequestFactory
-from django.core.files.uploadedfile import SimpleUploadedFile
-from rest_framework.test import APIRequestFactory
+import tempfile
+from django.urls import reverse
+from rest_framework.test import APITestCase
 from rest_framework import status
-from myapp.views import FileView
+from unittest.mock import Mock, patch
 
-class TestFileView(TestCase):
+
+class FileViewTestCase(APITestCase):
+
     def setUp(self):
-        self.factory = APIRequestFactory()
-        self.view = FileView.as_view()
-        self.file_data = {'name': 'test.txt', 'owner': 'testuser', 'size': 10, 'fike': SimpleUploadedFile('test.txt', b'test content')}
-        self.request = self.factory.post('/files/fudji', self.file_data)
-        self.request.user = 'testuser'
-        self.request.session = {}
+        self.instance_name = "FUDJI"
+        self.file_content = b"This is a test file."
+        self.file_name = "test.txt"
+        self.invalid_instance_name = "INVALID"
 
-    def test_post_success(self):
-        response = self.view(self.request, instance_name='FUDJI')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data, [{'status': 'success', 'file': 'test.txt', 'size': 10}])
+    def test_upload_file(self):
+        # Création d'un fichier temporaire pour les tests
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(self.file_content)
+            temp_file.seek(0)
+            # Création de l'objet fichier pour la requête POST
+            file_obj = Mock()
+            file_obj.name = self.file_name
+            file_obj.size = len(self.file_content)
+            file_obj.read = temp_file.read
 
-    def test_post_invalid_file(self):
-        invalid_data = {'name': 'test.txt', 'owner': 'testuser', 'size': 0, 'fike': None}
-        request = self.factory.post('/files/fudji', invalid_data)
-        request.user = 'testuser'
-        request.session = {}
-        response = self.view(request, instance_name='FUDJI')
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data, [{'status': 'failure'}])
+            # Envoi de la requête POST pour télécharger le fichier
+            url = reverse('report', kwargs={'instance_name': self.instance_name})
+            response = self.client.post(url, {'file': file_obj}, format='multipart')
 
-    def test_post_permission_denied(self):
-        request = self.factory.post('/files/fudji', self.file_data)
-        request.user = 'testuser'
-        request.session = {}
-        view = FileView.as_view(permission_classes=[])
-        response = view(request, instance_name='FUDJI')
+            # Vérification que la réponse est 201 CREATED
+            self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+            # Vérification que le fichier a été correctement téléchargé sur le bucket COS
+            # (pour cette partie du test, il est nécessaire de vérifier manuellement le bucket COS)
+            # ...
+
+    def test_upload_file_with_invalid_permissions(self):
+        # Envoi de la requête POST pour télécharger le fichier avec des permissions invalides
+        url = reverse('report', kwargs={'instance_name': self.instance_name})
+        response = self.client.post(url, {}, format='multipart')
+
+        # Vérification que la réponse est 403 FORBIDDEN
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_upload_file_with_invalid_instance_name(self):
+        # Création d'un fichier temporaire pour les tests
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(self.file_content)
+            temp_file.seek(0)
+            # Création de l'objet fichier pour la requête POST
+            file_obj = Mock()
+            file_obj.name = self.file_name
+            file_obj.size = len(self.file_content)
+            file_obj.read = temp_file.read
+
+            # Envoi de la requête POST pour télécharger le fichier avec un nom de bucket invalide
+            url = reverse('report', kwargs={'instance_name': self.invalid_instance_name})
+            response = self.client.post(url, {'file': file_obj}, format='multipart')
+
+            # Vérification que la réponse est 400 BAD REQUEST
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_download_file(self):
+        # Création d'un fichier temporaire pour les tests
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(self.file_content)
+            temp_file.seek(0)
+            # Envoi de la requête POST pour télécharger le fichier sur le bucket COS
+            url = reverse('report', kwargs={'instance_name': self.instance_name})
+            self.client.post(url, {'file': temp_file}, format='multipart')
+
+        # Envoi de la requête GET pour télécharger le fichier
+        url = reverse('report', kwargs={'instance_name': self.instance_name, 'file_name': self.file_name})
+        response
--- cos_release/api_v1/urls.py
+++ cos_release/api_v1/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import upload_file, download_file, delete_file
 
 urlpatterns = [
-    path('upload/', upload_file, name='upload_file'),
-    path('download/<str:file_name>/', download_file, name='download_file'),
-    path('delete/<str:file_name>/', delete_file,  name='delete_file'),
+    path('report/<str:instance_name>', FileView.as_view(), name='report'),
+    path('report/<str:instance_name>/<str:file_name>', FileView.as_view(), name='report'),
 ]
