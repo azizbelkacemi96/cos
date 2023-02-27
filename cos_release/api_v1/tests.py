d2ffc71675134698aad1e3c7206351c78c15afa0
aziz <azizz_belkacemi@yahoo.com>
2023-02-27 16:37:11 +0100

    test

--- cos_release/api_v1/tests.py
+++ cos_release/api_v1/tests.py
@@ -1,25 +1,39 @@
-from django.contrib.auth.models import User, Permission
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import User
+from rest_framework.test import force_authenticate
+from rest_framework import status
+from .views import FileView
+from .permissions import CanUploadFile, CanDownloadFile
 
-class MyTestCase(TestCase):
+
+class FileViewTestCase(TestCase):
+
     def setUp(self):
-        # Créez un utilisateur de test avec les permissions d'administrateur
+        self.factory = RequestFactory()
+        self.view = FileView.as_view()
         self.user = User.objects.create_user(
-            username='admin',
-            password='password',
-            is_staff=True,
-            is_superuser=True,
+            username='testuser',
+            password='testpass'
         )
 
-        # Obtenez les permissions d'administrateur pour les tests
-        self.ct_change_permission = Permission.objects.get(codename='change_mondelobjet')
+    def test_valid_file_upload_with_permission(self):
+        request = self.factory.post('/report/instance1', {'file': open('test_file.txt', 'rb')}, format='multipart')
+        force_authenticate(request, user=self.user)
+        response = self.view(request, instance_name='instance1')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        # Attribuez les permissions d'administrateur à l'utilisateur de test
-        self.user.user_permissions.add(self.ct_change_permission)
-        self.user.save()
+    def test_valid_file_upload_without_permission(self):
+        request = self.factory.post('/report/instance1', {'file': open('test_file.txt', 'rb')}, format='multipart')
+        response = self.view(request, instance_name='instance1')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_my_function(self):
-        # Utilisez l'utilisateur de test avec les permissions d'administrateur pour exécuter le test
-        self.client.force_login(self.user)
-        response = self.client.get('/mon_url/')
-        self.assertEqual(response.status_code, 200)
+    def test_invalid_file_upload_with_permission(self):
+        request = self.factory.post('/report/instance1', {'file': 'invalid_file'}, format='multipart')
+        force_authenticate(request, user=self.user)
+        response = self.view(request, instance_name='instance1')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_invalid_file_upload_without_permission(self):
+        request = self.factory.post('/report/instance1', {'file': 'invalid_file'}, format='multipart')
+        response = self.view(request, instance_name='instance1')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
--- cos_release/api_v1/views.py
+++ cos_release/api_v1/views.py
@@ -9,22 +9,59 @@ from .permissions import CanUploadFile, CanDownloadFile
 
 
 class FileView(APIView):
+    parser_classes = [MultiPartParser]
     permission_classes = [IsAuthenticated]
 
-    def get(self, request, bucket_name, file_name):
-        file = get_object_or_404(File, name=file_name, bucket__name=bucket_name)
+    def map_bucket_name():
+        if map_bucket_name.upper()=="FUDJI": return settings.COS_BUCKET_FUDJI
+        if map_bucket_name.upper()=="VESUVE": return settings.COS_BUCKET_VESUVE
+        if map_bucket_name.upper()=="ETNA": return settings.COS_BUCKET_ETNA
+        if map_bucket_name.upper()=="IZARU": return settings.COS_BUCKET_IZARU
 
-        if not CanDownloadFile().has_permission(request, self):
-            return HttpResponse(status=403)
-
-        # Implement file download logic here
-        # ...
-
-    def post(self, request, bucket_name, file_name):
-        if not CanUploadFile().has_permission(request, self):
-            return HttpResponse(status=403)
-
-        # Implement file upload logic here
-        # ...
-
-        return Response(status=status.HTTP_201_CREATED)
+    def post(self,request, instance_name, *args, **kwargs):
+        upload_files = []
+        files = request.FILES
+        error = False
+        bucket_name = self.map_bucket_name(instance_name)
+        if files:
+            if not CanUploadFile().has_permission(request, self):
+                return HttpResponse(f"mkjfbgjk")
+            for f in files.lists():
+                file = {
+                    "name": request.FILES[f[0]].name,
+                    "name": request.user,
+                    "name": request.FILES[f[0]].size,
+                    "name": request.FILES[f[0]],
+                }
+                serializer = FileSerializer(data=file)
+                if serializer.is_valid():
+                    try:
+                        serializer.save(bucket_name)
+                        upload_files.append(
+                            {
+                                "status": "succes",
+                                "file": request.FILES[f[0]],
+                                "size": request.FILES[f[0]].size,
+                            })
+                    except Exception as e:
+                        error = True
+                        upload_files.append(
+                            {
+                                "status": "error",
+                                "file": request.FILES[f[0]],
+                                "size": request.FILES[f[0]].size,
+                            })
+                else:
+                    error = True
+                    upload_files.append(
+                        {
+                            "status": "error",
+                            "file": request.FILES[f[0]],
+                            "size": request.FILES[f[0]].size,
+                        })
+            if error:
+                return Response(upload_files, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response(upload_files, status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_200_OK)
