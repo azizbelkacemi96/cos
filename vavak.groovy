026ff94eaa451b26faa99a5e0eb54bdcecdba209
azizbelkacemi96 <70583676+azizbelkacemi96@users.noreply.github.com>
2024-06-21 16:39:47 +0200

    Update vavak.groovy

--- vavak.groovy
+++ vavak.groovy
@@ -1,75 +1,51 @@
-import io.argoproj.argocd.v1alpha1.api.AppsApi
-import io.argoproj.argocd.v1alpha1.model.App
-import io.argoproj.argocd.v1alpha1.model.AppDestinationServer
-import io.argoproj.argocd.v1alpha1.model.AppProjectSpec
-import io.argoproj.argocd.v1alpha1.model.AppSourcePath
-import io.argoproj.argocd.v1alpha1.model.AppSpec
-import io.argoproj.argocd.v1alpha1.model.AppSyncPolicyAutomated
-import io.argoproj.argocd.v1alpha1.model.RepoCreds
-import io.kubernetes.client.util.Config
+import io.argoproj.argocd.client.ApiClient
+import io.argoproj.argocd.client.api.AppsApi
+import io.argoproj.argocd.client.model.AppProject
+import io.argoproj.argocd.client.model.HelmChart
+import io.argoproj.argocd.client.model.RepoCreds
+import io.argoproj.argocd.client.model.V1alpha1Application
+import io.argoproj.argocd.client.model.V1alpha1ApplicationSpec
+import io.argoproj.argocd.client.model.V1alpha1ApplicationSyncPolicy
 
 class ArgoCdHelmDeployer {
-    private String namespace
-    private String releaseName
-    private String chartPath
-    private String valuesFilePath
-    private String argocdServer
-    private String argocdToken
-    private String argocdProject
-    private String repoUrl
+    private ApiClient apiClient
+    private AppsApi appsApi
 
-    ArgoCdHelmDeployer(String namespace, String releaseName, String chartPath, String valuesFilePath, String argocdServer, String argocdToken, String argocdProject, String repoUrl) {
-        this.namespace = namespace
-        this.releaseName = releaseName
-        this.chartPath = chartPath
-        this.valuesFilePath = valuesFilePath
-        this.argocdServer = argocdServer
-        this.argocdToken = argocdToken
-        this.argocdProject = argocdProject
-        this.repoUrl = repoUrl
+    ArgoCdHelmDeployer(String argocdServer, String argocdToken) {
+        this.apiClient = new ApiClient()
+        this.apiClient.setBasePath(argocdServer)
+        this.apiClient.setAccessToken(argocdToken)
+        this.appsApi = new AppsApi(apiClient)
     }
 
-    def deploy() {
-        // Configurer le client Argo CD
-        ApiClient apiClient = Config.defaultClient()
-        apiClient.setBasePath(argocdServer)
-        apiClient.setAccessToken(argocdToken)
-        AppsApi appsApi = new AppsApi(apiClient)
+    void deploy(String namespace, String releaseName, String chartPath, String valuesFilePath, String argocdProject, String repoUrl) {
+        // Créer l'objet AppProject pour Argo CD
+        AppProject appProject = new AppProject()
+        appProject.setName(argocdProject)
 
-        // Créer l'objet App pour Argo CD
-        App app = new App()
-        AppSpec appSpec = new AppSpec()
-        AppProjectSpec projectSpec = new AppProjectSpec()
-        projectSpec.setName(argocdProject)
-        appSpec.setProject(projectSpec)
+        // Créer l'objet HelmChart pour Argo CD
+        HelmChart helmChart = new HelmChart()
+        helmChart.setChart(chartPath)
+        helmChart.setValues(valuesFilePath)
 
-        AppSourcePath sourcePath = new AppSourcePath()
-        sourcePath.setChart(chartPath)
-        sourcePath.setTargetRevision(valuesFilePath)
-        sourcePath.setRepoURL(repoUrl)
-        appSpec.setSource(sourcePath)
-
-        AppDestinationServer destinationServer = new AppDestinationServer()
-        destinationServer.setServer("https://kubernetes.default.svc")
-        destinationServer.setNamespace(namespace)
-        appSpec.setDestination(destinationServer)
-
-        AppSyncPolicyAutomated syncPolicy = new AppSyncPolicyAutomated()
-        syncPolicy.setPrune(true)
-        syncPolicy.setSelfHeal(true)
-        appSpec.setSyncPolicy(syncPolicy)
-
-        app.setSpec(appSpec)
+        // Créer l'objet V1alpha1Application pour Argo CD
+        V1alpha1Application app = new V1alpha1Application()
+        V1alpha1ApplicationSpec appSpec = new V1alpha1ApplicationSpec()
+        appSpec.setProject(appProject)
+        appSpec.setSource(new io.argoproj.argocd.client.model.V1alpha1ApplicationSource().helm(helmChart).repoURL(repoUrl))
+        appSpec.setDestination(new io.argoproj.argocd.client.model.V1alpha1ApplicationDestination().server("https://kubernetes.default.svc").namespace(namespace))
+        appSpec.setSyncPolicy(new V1alpha1ApplicationSyncPolicy().automated(new io.argoproj.argocd.client.model.V1alpha1ApplicationSyncPolicyAutomated().prune(true)))
         app.setMetadata(new io.kubernetes.client.openapi.models.V1ObjectMeta().name(releaseName))
+        app.setSpec(appSpec)
 
         // Créer ou mettre à jour l'application Argo CD
         try {
-            appsApi.createApp(app)
+            appsApi.createApplication(app)
             println("L'application Argo CD a été créée avec succès.")
         } catch (ApiException e) {
             if (e.getCode() == 409) {
                 // L'application existe déjà, la mettre à jour
-                appsApi.replaceApp(releaseName, app)
+                appsApi.updateApplication(releaseName, app)
                 println("L'application Argo CD a été mise à jour avec succès.")
             } else {
                 throw e
