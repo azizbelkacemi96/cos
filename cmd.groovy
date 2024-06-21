13c6441f3a8adc44c2c5967722256124ee59a733
azizbelkacemi96 <70583676+azizbelkacemi96@users.noreply.github.com>
2024-06-21 17:46:09 +0200

    Update cmd.groovy

--- cmd.groovy
+++ cmd.groovy
@@ -2,19 +2,30 @@ class ArgoCDHelmDeployer {
     String argoServer
     String argoToken
     String helmChartPath
+    String helmValuesPath
     String helmReleaseName
     String helmNamespace
+    String helmVersion
+    Map<String, Object> helmValues
+    String syncPolicy
 
-    ArgoCDHelmDeployer(String argoServer, String argoToken, String helmChartPath, String helmReleaseName, String helmNamespace) {
+    ArgoCDHelmDeployer(String argoServer, String argoToken, String helmChartPath, String helmValuesPath, String helmReleaseName, String helmNamespace, String helmVersion, Map<String, Object> helmValues, String syncPolicy) {
         this.argoServer = argoServer
         this.argoToken = argoToken
         this.helmChartPath = helmChartPath
+        this.helmValuesPath = helmValuesPath
         this.helmReleaseName = helmReleaseName
         this.helmNamespace = helmNamespace
+        this.helmVersion = helmVersion
+        this.helmValues = helmValues
+        this.syncPolicy = syncPolicy
     }
 
     def deploy() {
-        def command = "argocd app create ${helmReleaseName} --repo ${helmChartPath} --path . --dest-namespace ${helmNamespace} --dest-server ${argoServer}"
+        def command = "argocd app create ${helmReleaseName} --repo ${helmChartPath} --path . --values ${helmValuesPath} --dest-namespace ${helmNamespace} --dest-server ${argoServer} --helm-set-string helmVersion=${helmVersion} --sync-policy ${syncPolicy} --auth-token ${argoToken}"
+        helmValues.each { key, value ->
+            command += " --helm-set-string ${key}=${value}"
+        }
         def process = command.execute()
         process.waitFor()
 
