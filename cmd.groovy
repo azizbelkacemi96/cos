fd0226e2c649019ff24b3d823524095460f19c06
azizbelkacemi96 <70583676+azizbelkacemi96@users.noreply.github.com>
2024-06-24 16:21:18 +0200

    Update cmd.groovy

--- cmd.groovy
+++ cmd.groovy
@@ -1,38 +1,13 @@
-class ArgoCDHelmDeployer {
-    String argoServer
-    String argoToken
-    String helmChartPath
-    String helmValuesPath
-    String helmReleaseName
-    String helmNamespace
-    String helmVersion
-    Map<String, Object> helmValues
-    String syncPolicy
-
-    ArgoCDHelmDeployer(String argoServer, String argoToken, String helmChartPath, String helmValuesPath, String helmReleaseName, String helmNamespace, String helmVersion, Map<String, Object> helmValues, String syncPolicy) {
-        this.argoServer = argoServer
-        this.argoToken = argoToken
-        this.helmChartPath = helmChartPath
-        this.helmValuesPath = helmValuesPath
-        this.helmReleaseName = helmReleaseName
-        this.helmNamespace = helmNamespace
-        this.helmVersion = helmVersion
-        this.helmValues = helmValues
-        this.syncPolicy = syncPolicy
+def deploy(String server, String project, String name, String revision, String path, String destServer, String destNamespace, List<String> helmValuesFiles) {
+    def cmd = "argocd app create ${name} --server ${server} --project ${project} --revision ${revision} --path ${path} --dest-server ${destServer} --dest-namespace ${destNamespace}"
+    if (helmValuesFiles) {
+        helmValuesFiles.each { file ->
+            cmd += " --helm-values-file ${file}"
+        }
     }
-
-    def deploy() {
-        def command = "argocd app create ${helmReleaseName} --repo ${helmChartPath} --path . --values ${helmValuesPath} --dest-namespace ${helmNamespace} --dest-server ${argoServer} --helm-set-string helmVersion=${helmVersion} --sync-policy ${syncPolicy} --auth-token ${argoToken}"
-        helmValues.each { key, value ->
-            command += " --helm-set-string ${key}=${value}"
-        }
-        def process = command.execute()
-        process.waitFor()
-
-        if(process.exitValue() == 0) {
-            println "Application created successfully"
-        } else {
-            println "Error in creating application: ${process.err.text}"
-        }
+    def process = cmd.execute()
+    process.waitFor()
+    if (process.exitValue() != 0) {
+        throw new RuntimeException("La commande Argo CD a échoué avec le code de sortie ${process.exitValue()}")
     }
 }
